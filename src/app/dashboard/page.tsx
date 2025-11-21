c7462946b9b04349ff0183facb760a2129b04463
Iván Ramos Flores <airamos.tech@gmail.com>
2025-11-20 21:52:01 -0500

    Fix: dashboard module export added

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl">Panel del usuario</h1>
+    </div>
+  );
+}
