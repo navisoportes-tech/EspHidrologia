a526720e698d73a6d04e343648a46c40aef2db6c
Iván Ramos Flores <airamos.tech@gmail.com>
2025-11-20 22:19:40 -0500

    Add message to login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,10 @@
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100">
-      <h1 className="text-3xl font-bold">Página de Login</h1>
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4">Página de Login</h1>
+        <p className="text-2xl text-blue-600 font-semibold">Hola Ivan eres un genio</p>
+      </div>
     </div>
   );
 }
