8f86b1984872ee162f53b0f5fdf77cea1969f353
Iván Ramos Flores <airamos.tech@gmail.com>
2025-11-20 22:16:48 -0500

    Remove dashboard route

--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function DashboardPage() {
-  return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-3xl">Panel del usuario</h1>
-    </div>
-  );
-}
